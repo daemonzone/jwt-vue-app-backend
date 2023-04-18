00f62341cd8c9bf679d05ccb9f7c46af0e87f15f
Davide <davide@daemonzone.it>
2023-04-18 19:46:58 +0200

    Adding postman requests collection

--- app/controllers/private_controller.rb
+++ app/controllers/private_controller.rb
@@ -4,6 +4,6 @@ class PrivateController < ApplicationController
   def test
     render json: {
       message: "This is a private message for #{current_user.email} you should only see if you've got a correct token"
-    }
+    }, status: :ok
   end
 end
