8890e1fa3868f0487205f612b3270954bc489136
Davide <davide@daemonzone.it>
2023-05-08 11:46:53 +0200

    Added rubocop and run rubocop -a (autocorrect)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::API
-  
   def users
     render json: User.all, status: :ok
   end
-
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
-  def respond_with(resource, _opts = {})
+  def respond_with(_resource, _opts = {})
     render json: current_user, status: :ok
   end
 
@@ -14,10 +14,10 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def log_out_success
-    render json: { message: "Logged out." }, status: :ok
+    render json: { message: 'Logged out.' }, status: :ok
   end
 
   def log_out_failure
-    render json: { message: "Logged out failure."}, status: :unauthorized
+    render json: { message: 'Logged out failure.' }, status: :unauthorized
   end
 end
